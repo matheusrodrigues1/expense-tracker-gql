2780b5c94ffccbf63c5626a2cec50ec20024b894
matheusrodrigues1 <matech00@outlook.com>
2024-03-14 17:22:51 -0300

    Database Setup(MongoDB)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from 'dotenv'
+
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
 import { expressMiddleware } from "@apollo/server/express4";
@@ -8,7 +10,9 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 
 import mergedResolvers from "./resolvers/index.js"
 import mergedTypeDefs from "./typeDefs/index.js"
+import { connectDB } from "./db/connectdb.js";
 
+dotenv.config()
 const app = express();
 
 const httpServer = http.createServer(app);
@@ -37,5 +41,6 @@ app.use(
 
   // Modified server startup
   await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+	await connectDB()
 
   console.log(`🚀 Server ready at http://localhost:4000/`);
