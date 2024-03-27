b19a5506a2f62a9d189aa54f141b0aec8c5deae8
matheusrodrigues1 <matech00@outlook.com>
2024-03-26 22:39:58 -0300

    3- Implementing Authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,35 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import TransactionPage from "./pages/TransactionPage";
-import Header from "./components/ui/Header";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import TransactionPage from "./pages/TransactionPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import Header from "./components/ui/Header.jsx";
+import { useQuery } from "@apollo/client";
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-	const authUser = true;
+	const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+
+	if (loading) return null;
+
 	return (
 		<>
-			{authUser && <Header />}
+			{data?.authUser && <Header />}
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/transaction/:id' element={<TransactionPage />} />
-				<Route path='*' element={<NotFound />} />
+				<Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route
+					path='/transaction/:id'
+					element={data.authUser ? <TransactionPage /> : <Navigate to='/login' />}
+				/>
+				<Route path='*' element={<NotFoundPage />} />
 			</Routes>
+			<Toaster />
 		</>
 	);
 }
+
 export default App;
